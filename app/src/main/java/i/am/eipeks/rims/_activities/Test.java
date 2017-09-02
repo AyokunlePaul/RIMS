7120af9fa8a7348f5967206bf36f14891949a2aa
Eipeks <apexzdgr8est@gmail.com>
2017-09-02 22:44:40 +0100

    Update

---
+++ app/src/main/java/i/am/eipeks/rims/_activities/Test.java
@@ -0,0 +1,15 @@
+package i.am.eipeks.rims._activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import i.am.eipeks.rims.R;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
